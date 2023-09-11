2dd122c8f28fdd1edb072a1d33f3f1e9000513e3
misha133kv <mihail_fomin_95@mail.ru>
2023-09-11 19:35:22 +0500

    added register and login routes

---
+++ index.js
@@ -0,0 +1,131 @@
+import express, { response } from 'express'
+import config from 'config'
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+import { registerValidation } from './validations/auth.js'
+import UserModel from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
+
+mongoose
+  .connect(config.get('MONGO_DB_PATH'))
+  .then(() => console.log('DB OK!'))
+  .catch(err => console.log('DB error: ', err))
+
+const app = express();
+
+app.use(express.json())
+
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'Пользователь не найден'
+      })
+    }
+
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+    if (!isValidPass) {
+      return res.status(404).json({
+        message: 'Неверный логин или пароль'
+      })
+    }
+
+    const token = jwt.sign({
+      _id: user._id,
+    },
+      'secret123',
+      {
+        expiresIn: '30d',
+      }
+    )
+
+    const { passwardHash, ...userData } = user._doc
+
+    res.json({ ...userData, token, })
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Didn\'t manage to login'
+    })
+  }
+})
+
+app.post('/auth/register', registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array())
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      password: req.body.password,
+      passwordHash: hash,
+    })
+
+    const user = await doc.save();
+
+    const token = jwt.sign({
+      _id: user._id,
+    },
+      'secret123',
+      {
+        expiresIn: '30d',
+      }
+    )
+
+    const { passwardHash, ...userData } = user._doc
+
+    res.json({ ...userData, token, })
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Didn\'t manage to register'
+    })
+  }
+
+})
+
+app.get('/auth/me', checkAuth, async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.userId)
+    console.log('user: ', user);
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found'
+      })
+    }
+
+    const { passwardHash, ...userData } = user._doc
+
+    res.json(userData)
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Didn\'t manage to login'
+    })
+  }
+})
+
+app.listen(4444, err => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Server OK!');
+
+});
