d86200266fc7ce1d6966723ac637d10acfb7662e
misha133kv <mihail_fomin_95@mail.ru>
2023-09-14 21:26:30 +0500

    fix populate user

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,8 +2,34 @@ import PostModel from '../models/Post.js'
 
 export const getAll = async (req, res) => {
   try {
-    const posts = await PostModel.find().populate('user');
+    const posts = await PostModel.find().populate({ path: 'user', select: ["fullName", "avatarUrl"] });
     res.json(posts)
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Didn\'t manage to get an article'
+    })
+  }
+}
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id
+
+    const doc = await PostModel.findOneAndUpdate(
+      { _id: postId },
+      { $inc: { viewsCount: 1 } },
+      { returnDocument: 'after', }
+    )
+
+    if (!doc) {
+      return res.status(404).json({
+        message: 'An article was not found'
+      })
+    }
+
+    res.json(doc)
   } catch (error) {
     console.log(error);
 
