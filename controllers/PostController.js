31be3000226a24c68a5cb3108ff6b682bbc6142d
misha133kv <mihail_fomin_95@mail.ru>
2023-09-15 08:53:54 +0500

    added remove method

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -60,3 +60,33 @@ export const create = async (req, res) => {
     })
   }
 }
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id
+    if (!postId) {
+      return res.status(404).json({
+        message: 'An article was not found'
+      })
+    }
+    const doc = await PostModel.findByIdAndDelete({
+      _id: postId
+    })
+
+    if (!doc) {
+      return res.status(404).json({
+        message: 'An article was not found'
+      })
+    }
+
+    res.json({
+      success: true
+    })
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Didn\'t manage to remove an article'
+    })
+  }
+}
