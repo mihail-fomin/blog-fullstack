df30ad0e0312c756acdf4bbec208c9b4a601efba
misha133kv <mihail_fomin_95@mail.ru>
2023-10-08 15:25:42 +0500

    new port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import checkAuth from './utils/checkAuth.js'
 import { UserController, PostController } from './controllers/index.js'
 
 mongoose
-  .connect(config.get('MONGO_DB_PATH'))
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('DB OK!'))
   .catch(err => console.log('DB error: ', err))
 
@@ -57,7 +57,7 @@ app.delete('/posts/:id', checkAuth, PostController.remove)
 app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update)
 
 
-app.listen(4444, err => {
+app.listen(process.env.PORT || 4444, err => {
   if (err) {
     return console.log(err);
   }
